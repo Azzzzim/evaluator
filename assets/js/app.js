bdda045e2627f2631d41482c8599b5bb96f42b6b
CECK <97340544+Azzzzim@users.noreply.github.com>
2022-02-24 11:09:45 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+let op;
+
+function func() {
+  let result;
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+  switch (op) {
+    case "+":
+      result = "прибовлять захотелось? пососи!";
+      break;
+    case "-":
+      result = "вычетать захотелось? иди нахуй!";
+      break;
+
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+  }
+  document.getElementById("result").innerHTML = result;
+  
+
+
+  
+  
+}
